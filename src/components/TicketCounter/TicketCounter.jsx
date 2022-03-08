48d7a93963b273d092bc8000629141245d85e6b6
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-08 14:37:53 +0000

    Made TicketCounter component

--- src/components/TicketCounter/TicketCounter.jsx
+++ src/components/TicketCounter/TicketCounter.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import "./TicketCounter.scss"
+import "./TicketCounter.scss";
+import { useState } from "react";
 
 const TicketCounter = () => {
-  return (
-    <div>TicketCounter</div>
-  )
-}
+  const [counter, setCounter] = useState(0);
 
-export default TicketCounter
+  const handleDecrement = () => {
+    console.log("handling decrement");
+    setCounter(counter - 1);
+  };
+
+  const handleIncrement = () => {
+    console.log("handling increment");
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div className="ticket-counter">
+      <button onClick={handleDecrement}>Decrement</button>
+      <p>{counter}</p>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+};
+
+export default TicketCounter;
