ad50c88c73231c3b04cc49a7d9a2275cf01ddb5c
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-09 15:51:59 +0000

    Added key to employee cards

--- src/components/EmployeeCards/EmployeeCards.jsx
+++ src/components/EmployeeCards/EmployeeCards.jsx
@@ -3,10 +3,10 @@ import TicketCounter from '../TicketCounter/TicketCounter'
 import "./EmployeeCards.scss"
 
 const EmployeeCards = ({teamArr}) => {
-  const employeeCardsJSX = teamArr.map(({name,role}) => {
+  const employeeCardsJSX = teamArr.map(({name,role,id}) => {
     return (
       <>
-        <div className="employee-tile">
+        <div key={id} className="employee-tile">
           <div className="employee-tile__info">
             <p className="employee-tile__info--name"><span className="prefix">Name:</span> {name}</p>
             <p className="employee-tile__info--role"><span className="prefix">Role:</span> {role}</p>
