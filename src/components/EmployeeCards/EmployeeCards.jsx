45a5db5f5dc3936a294ff29d2c775b373e524c27
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-09 15:53:45 +0000

    Renamed some classes

--- src/components/EmployeeCards/EmployeeCards.jsx
+++ src/components/EmployeeCards/EmployeeCards.jsx
@@ -6,10 +6,10 @@ const EmployeeCards = ({teamArr}) => {
   const employeeCardsJSX = teamArr.map(({name,role,id}) => {
     return (
       <>
-        <div key={id} className="employee-tile">
-          <div className="employee-tile__info">
-            <p className="employee-tile__info--name"><span className="prefix">Name:</span> {name}</p>
-            <p className="employee-tile__info--role"><span className="prefix">Role:</span> {role}</p>
+        <div key={id} className="employee-card">
+          <div className="employee-card__info">
+            <p className="employee-card__info--name"><span className="prefix">Name:</span> {name}</p>
+            <p className="employee-card__info--role"><span className="prefix">Role:</span> {role}</p>
           </div>
           <TicketCounter />
         </div>
